222d5722711704148b3f64cf72d16e92bfed45e6
rakibfiha <mrahman06@qub.ac.uk>
2024-02-14 20:26:21 +0900

    copilot: config.lua

--- config/lvim/config.lua
+++ config/lvim/config.lua
@@ -10,3 +10,39 @@ vim.api.nvim_create_autocmd({"VimEnter"}, {callback = open_nvim_tree})
 
 lvim.icons.ui.Folder = "󰉋"
 lvim.builtin.nvimtree.setup.renderer.icons.glyphs.folder.default = lvim.icons.ui.Folder
+
+lvim.plugins = {
+{
+"zbirenbaum/copilot.lua",
+cmd = "Copilot",
+event = "InsertEnter",
+config = function()
+require("copilot").setup({})
+end,
+},
+{
+"zbirenbaum/copilot-cmp",
+config = function ()
+require("copilot_cmp").setup({
+suggestion = { enabled = false },
+panel = { enabled = false }
+})
+end
+}
+}
+-- Below config is required to prevent copilot overriding Tab with a suggestion
+-- when you're just trying to indent!
+local has_words_before = function()
+if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then return false end
+local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+return col ~= 0 and vim.api.nvim_buf_get_text(0, line-1, 0, line-1, col, {})[1]:match("^%s*$") == nil
+end
+local on_tab = vim.schedule_wrap(function(fallback)
+local cmp = require("cmp")
+if cmp.visible() and has_words_before() then
+cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+else
+fallback()
+end
+end)
+lvim.builtin.cmp.mapping["<Tab>"] = on_tab
