fecffcba5ae9f4d3b32378489e87a53353843083
rakibfiha <mrahman06@qub.ac.uk>
2024-02-27 20:18:11 +0900

    vim: remove open file explorer at beginning

--- config/lvim/config.lua
+++ config/lvim/config.lua
@@ -3,10 +3,6 @@
 -- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
 -- Forum: https://www.reddit.com/r/lunarvim/
 -- Discord: https://discord.com/invite/Xb9B4Ny
-local function open_nvim_tree()
-require("nvim-tree.api").tree.open()
-end
-vim.api.nvim_create_autocmd({"VimEnter"}, {callback = open_nvim_tree})
 
 lvim.icons.ui.Folder = "󰉋"
 lvim.builtin.nvimtree.setup.renderer.icons.glyphs.folder.default = lvim.icons.ui.Folder
